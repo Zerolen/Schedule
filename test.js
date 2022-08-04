0c50f3292a69347c2bf174f342417ef9af1da723
4g-eater <86425326+Starkoans@users.noreply.github.com>
2022-08-04 21:23:51 +0700

    test

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ const test = require('tape');
 // Start the app
 const env = Object.assign({}, process.env, {PORT: 5000});
 const child = spawn('node', ['index.js'], {env});
-
+//123
 test('responds to requests', (t) => {
   t.plan(4);
 
