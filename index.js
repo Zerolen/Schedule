3ef428d27aa876c95ff3e52f422613e3492c4623
Zerolen <Sandtius@gmail.com>
2022-08-07 01:06:04 +0700

    Ebal GiT

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-import mongoose from 'mongoose'
-//test comment
+import mongoose from 'mongoose';
+import express from 'express';
+
+
 mongoose.connect('mongodb+srv://Admin:Admin@cluster0.ccd0lek.mongodb.net/Test123?retryWrites=true&w=majority', )
 .then(() => console.log('DB OK'))
 .catch((err) => console.log('DB error', err));
 
-//tut byl stes
+
