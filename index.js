1727560cf92d178b2f3f69783c6f533e198a783e
4g-eater <86425326+Starkoans@users.noreply.github.com>
2022-08-06 17:24:07 +0700

    testin chages

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import mongoose from 'mongoose'
-
-
+//test comment
 mongoose.connect('mongodb+srv://Admin:Admin@cluster0.ccd0lek.mongodb.net/Test123?retryWrites=true&w=majority', )
 .then(() => console.log('DB OK'))
 .catch((err) => console.log('DB error', err));
 
-
+//tut byl stes
