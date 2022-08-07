af16a215ac3c53741bb476e5af0cfed08ce4a9ac
Nastas <tasya16mosya@gmail.com>
2022-08-07 14:31:15 +0700

    bebr

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
 import mongoose from 'mongoose';
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import req from "express/lib/request.js";
+
+const app = express();
+//приложение читает json-запросы
+ app.use(express.json());
+
+//get-запрос на главную страницу
+app.get('/',(req, res) =>{
+    res.send('Тут будет распа.');//выгрузка в html
+    console.log("get ok");
+
+} );
+//post-запрос на страницу /логин
+app.post('/login', (rec,res)=>{
+    //вывод тела запроса в консоль
+    console.log(req.body);
+
+    // const token = jwt.sign(
+    //     {
+    //         email: req.body.email,
+    //         nickname : 'Bebra777'
+    //     }, 'superkey586'
+    // )
+
+    //описание тела респонса от сервера
+    res.json({
+        success: true,
+
+    })
+});
+
+
+app.listen(4443, (err)=> {
+    if (err){ return(console.log(err))}
+    console.log('Server OK');
+
+})
 
 
 mongoose.connect('mongodb+srv://Admin:Admin@cluster0.ccd0lek.mongodb.net/Test123?retryWrites=true&w=majority', )
